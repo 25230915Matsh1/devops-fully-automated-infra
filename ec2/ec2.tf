7c526caa2982477e6c3d200bf314023323ae2966
masixole <masixole69@yahoo.com>
2023-12-20 12:31:26 +0000

    update

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -49,7 +49,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
